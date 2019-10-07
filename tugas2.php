71bf04211668db82fb8b6330c55eb4327f136a44
yohanes_prima <jooprima@gmail.com>
2019-10-07 10:44:17 +0700

    Tugas  2 PHP
    tugas ini dikumpulkan untuk niomic

---
+++ tugas2.php
@@ -0,0 +1,11 @@
+<?php
+$a=36;
+$b=12;
+$c=24;
+$d=($a*$b)/$c;
+
+echo "$a Hari = $b Pekerja <br>";
+echo "X hari = $c Pekerja <br>";
+echo "X = ...?<br>";
+echo "Jawaban : $d hari";
+ ?>
---
+++ tugas2_2.php
@@ -0,0 +1,10 @@
+<?php
+$x=80000;
+$y=50000;
+$z=($x-$y)/$y*100;
+
+echo "jual = $x <br>
+      Beli = $y <br>
+      Keuntungan = ? <br>
+      Jawaban : $z %";
+ ?>
